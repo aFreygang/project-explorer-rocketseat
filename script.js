450d3d8b0b380f0c988a45fa498edf7735f17c12
André Luiz Freygang <72108085+aFreygang@users.noreply.github.com>
2022-07-05 22:01:05 -0300

    added script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let mudar_base_cor = document.documentElement
+var cores = [
+  'light-theme-blue',
+  'light-theme-pink',
+  'dark-theme-blue',
+  'dark-theme-pink'
+]
+let listas_cores = document.querySelectorAll('.alterar-cores')
+
+listas_cores.addEventListener('mousemove', {
+  cores.forEach(cor => {
+    if (condition) {
+      
+    }
+  })
+})
