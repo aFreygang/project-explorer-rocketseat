846d7bd5bad9ccc10c3b9fb76a42de00cd441cd3
André Luiz Freygang <72108085+aFreygang@users.noreply.github.com>
2022-07-06 21:35:52 -0300

    insert js to change theme

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let mudar_base_cor = document.documentElement
-var cores = [
-  'light-theme-blue',
-  'light-theme-pink',
-  'dark-theme-blue',
-  'dark-theme-pink'
-]
-let listas_cores = document.querySelectorAll('.alterar-cores')
+const body_page = document.querySelector('body')
+const change_colors = document.querySelectorAll('ul .alterar-cores')
 
-listas_cores.addEventListener('mousemove', {
-  cores.forEach(cor => {
-    if (condition) {
-      
+for (const element of change_colors) {
+  element.addEventListener('click', function () {
+    if (element.id === 'light-theme-pink') {
+      body_page.className = ''
+      body_page.classList.toggle('light-pink')
+    } else if (element.id === 'dark-theme-blue') {
+      body_page.className = ''
+      body_page.classList.toggle('dark-blue')
+    } else if (element.id === 'dark-theme-pink') {
+      body_page.className = ''
+      body_page.classList.toggle('dark-pink')
+    } else {
+      body_page.className = ''
     }
   })
-})
+}
